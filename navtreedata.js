1c02f8a6bf7d08718066f6953accda8efae5d6ad
heller77 <heller77@users.noreply.github.com>
2024-06-10 14:07:03 +0000

    deploy: f2055af33794e91f0dedfafca9ec0415083007bc

--- navtreedata.js
+++ navtreedata.js
@@ -24,30 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "SimpleFPS", "index.html", [
-    [ "Packages", "namespaces.html", [
-      [ "Package List", "namespaces.html", "namespaces_dup" ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ],
-        [ "Properties", "functions_prop.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
-    ] ]
-  ] ]
+  [ "SimpleFPS", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"Armor_8cs.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
