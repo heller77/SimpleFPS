dc85e7f870c39867f9b8323b8fcf30a2493a9aaa
heller77 <koki001230@icloud.com>
2024-06-23 22:27:32 +0900

    objectのid用のクラスを作成

---
+++ Assets/Scripts/ObjectIDs/ObjectID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectIDs
+{
+    public class ObjectID
+    {
+        private Guid id;
+
+        public Guid GetID()
+        {
+            return this.id;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectIDs/ObjectIdentify.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ObjectIDs
+{
+    /// <summary>
+    /// オブジェクトの識別子
+    /// </summary>
+    public class ObjectIdentify : MonoBehaviour
+    {
+        private ObjectIDs.ObjectID _idref;
+
+        /// <summary>
+        /// id取得
+        /// </summary>
+        /// <returns></returns>
+        public ObjectID GetID()
+        {
+            return _idref;
+        }
+    }
+}
