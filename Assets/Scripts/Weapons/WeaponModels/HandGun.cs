06e2669468ede4c1ea29e0668d1f9c441716987d
heller77 <koki001230@icloud.com>
2024-06-24 14:41:33 +0900

    撃った弾が何かに当たったらhitmanagerに通知するように

--- Assets/Scripts/Weapons/WeaponModels/HandGun.cs
+++ Assets/Scripts/Weapons/WeaponModels/HandGun.cs
@@ -1,4 +1,5 @@
-﻿using R3;
+﻿using Managers;
+using R3;
 using UnityEngine;
 using Weapons.Bullets;
 
@@ -6,11 +7,20 @@ namespace Weapons
 {
     public class HandGun : MonoBehaviour, IAttackable, IWeapon
     {
+        private HitManager _hitManager;
+
+        public void Initialize(HitManager hitManager)
+        {
+            this._hitManager = hitManager;
+        }
+
         public void Attack()
         {
             // Debug.Log("HandGun Attack!!");
             //弾を生成、
-            BulletController._instance?.FireBullet(this.transform.position, Vector3.forward.normalized * 0.1f);
+            var bulletMono =
+                BulletController._instance?.FireBullet(this.transform.position, Vector3.forward.normalized * 0.1f);
+            bulletMono?.HitEvent.Subscribe(HitInfo => { _hitManager.ProcessHit(HitInfo); });
         }
     }
 }
